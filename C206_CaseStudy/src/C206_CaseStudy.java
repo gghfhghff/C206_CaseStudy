8363c11ec72a6117150d4baa35698c2a7d274e91
21016412 <21016412@myrp.edu.sg>
2022-08-02 17:34:11 +0800

    stuff

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,3 +7,66 @@ public class C206_CaseStudy {
 	}
 
 }
+
+/*
+static ArrayList<Category> categoryList = new ArrayList<Category>();
+
+String a = viewCategories(categoryList);
+System.out.println(a);
+
+
+int id = categoryList.size() + 1;
+String name = Helper.readString("Enter Category name: ");
+String details = Helper.readString("Enter Category details: ");
+
+Category category = new Category(id, name, details, cca);
+addCategories(categoryList, category);
+
+
+int id = Helper.readInt("Enter ID: ");
+deleteCategories(categoryList, id);
+
+
+
+public static String viewCategories(ArrayList<Category> categoryList) {
+	String output = String.format("%-5s %-20s %-20s\n", "ID", "NAME", "DETAILS");
+
+	for (int i = 0; i < categoryList.size(); i++) {
+		if (!categoryList.get(i).getName().isEmpty()) {
+			output += String.format("%-5d %-20s %-20s\n", categoryList.get(i).getId(),
+					categoryList.get(i).getName(), categoryList.get(i).getDetails());
+		}
+	}
+	return output;
+
+}
+
+
+
+public static void addCategories(ArrayList<Category> categoryList, Category category) {
+	if (category.getName().isEmpty() || category.getDetails().isEmpty()) {
+		System.out.println("Please enter a valid input!");
+	} else {
+		categoryList.add(category);
+		System.out.println("Successfully added category!");
+	}
+}
+
+
+
+public static void deleteCategories(ArrayList<Category> categoryList, int id) {
+	boolean valid = false;
+	for (int i = 0; i < categoryList.size(); i++) {
+		if (categoryList.get(i).getId() == id) {
+			categoryList.remove((i));
+			valid = true;
+			System.out.println("Successfully deleted category!");
+			break;
+		}
+	}
+	
+	if (valid == false) {
+		System.out.println("Invalid ID!");
+	}
+}
+*/
